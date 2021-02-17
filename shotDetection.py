ab2af616f4a1810a4a71b8762caa0a8a25c611d6
Caden Kroonenberg <cskroonenberg@gmail.com>
2021-02-17 12:30:01 -0600

    Fix error in shot detection function which would stop user input from affecting the shot's column choice

--- shotDetection.py
+++ shotDetection.py
@@ -32,43 +32,48 @@ p2shotArr = [
 def shot(player):
     if player == 1:
         shotArr = p1shotArr
-        shipArr = shipPlacement.p2shipArr
+        enemyShipArr = shipPlacement.p2shipArr
     else:
         shotArr = p2shotArr
-        shipArr = shipPlacement.p1shipArr
+        enemyShipArr = shipPlacement.p1shipArr
     
+    #shot selection
     repeat = True
     while repeat == True:
         repeat = False
         xChar = input('Enter a column [A-J] to fire upon: ')
-        if xChar == 'A' or 'a':
+        print("xChar:", xChar)
+        if xChar == "A" or xChar == "a":
             xCoord = 0
-        elif xChar == 'B' or 'b':
+        elif xChar == "B" or xChar == "b":
             xCoord = 1
-        elif xChar == 'C' or 'c':
+        elif xChar == "C" or xChar == "c":
             xCoord = 2
-        elif xChar == 'D' or 'd':
+        elif xChar == "D" or xChar == "d":
             xCoord = 3
-        elif xChar == 'E' or 'e':
+        elif xChar == "E" or xChar == "e":
             xCoord = 4
-        elif xChar == 'F' or 'f':
+        elif xChar == "F" or xChar == "f":
             xCoord = 5
-        elif xChar == 'G' or 'g':
+        elif xChar == "G" or xChar == "g":
             xCoord = 6
-        elif xChar == 'H' or 'h':
+        elif xChar == "H" or xChar == "h":
             xCoord = 7
-        elif xChar == 'I' or 'i':
+        elif xChar == "I" or xChar == "i":
             xCoord = 8
-        elif xChar == 'J' or 'j':
+        elif xChar == "J" or xChar == "j":
             xCoord = 9
         else:
             print("Invalid input. Try again")
             repeat = True
-    yCoord = int(input('Enter a row [1-10] to fire upon: ')) - 1
-    shotArr[xCoord][yCoord] = 1
+    yCoord = int(input('Enter a row [1-10] to fire upon: '))
+    yCoord = yCoord - 1
+
+    #register shot
+    shotArr[yCoord][xCoord] = 1
 
     #shot feedback
-    if shipArr[xCoord][yCoord] == shotArr[xCoord][yCoord]:
+    if enemyShipArr[yCoord][xCoord] == shotArr[yCoord][xCoord]:
         print("Shot hit!")
     else:
         print("Shot missed.")
