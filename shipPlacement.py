ccd02ba86095f793fde2f87f27d87da638d28186
Caden Kroonenberg <cskroonenberg@gmail.com>
2021-02-17 17:01:39 -0600

    Resolve issue with ship overlap detection which would raise an exception if Player 1's ships overlapped with Player 2's ships.

--- shipPlacement.py
+++ shipPlacement.py
@@ -71,41 +71,48 @@ def placeBody(parts, shipArr):
 def placeShip(player, shipCount):
     #player 1 = 1, player 2 = 2
     parts = []
-    for shipBodyPart in range(1, shipCount +1):
-        print('Place the #' + str(shipBodyPart) + 'of' + str(shipCount) + 'of the ship')
-        #A-J and 1-10 notation for consistency
-        repeat = True
-        while repeat == True:
-            repeat = False
-            xChar = input('Enter a column [A-J] to fire upon: ')
-            print("xChar:", xChar)
-            if xChar == "A" or xChar == "a":
-                xCoord = 0
-            elif xChar == "B" or xChar == "b":
-                xCoord = 1
-            elif xChar == "C" or xChar == "c":
-                xCoord = 2
-            elif xChar == "D" or xChar == "d":
-                xCoord = 3
-            elif xChar == "E" or xChar == "e":
-                xCoord = 4
-            elif xChar == "F" or xChar == "f":
-                xCoord = 5
-            elif xChar == "G" or xChar == "g":
-                xCoord = 6
-            elif xChar == "H" or xChar == "h":
-                xCoord = 7
-            elif xChar == "I" or xChar == "i":
-                xCoord = 8
-            elif xChar == "J" or xChar == "j":
-                xCoord = 9
-            else:
-                print("Invalid input. Try again")
-                repeat = True
-        yCoord = int(input('Enter a row [1-10] to fire upon: '))
-        yCoord = yCoord - 1
-        parts.append((xCoord, yCoord))
-    parts = canonicalizeParts(parts)
+
+    #one iteration per ship
+    for i in range (1, shipCount +1):
+        print("Player", player, "Ship", i, ":")
+        #one iteration per ship part
+        for shipBodyPart in range(1, i+1):
+            print('Place part ' + str(shipBodyPart) + ' of ' + str(i) + ' of the [1x' + str(i) + '] ship')
+            #A-J and 1-10 notation for consistency
+            repeat = True
+            while repeat == True:
+                repeat = False
+                xChar = input('Enter a column [A-J] to place ship: ')
+                if xChar == "A" or xChar == "a":
+                    xCoord = 0
+                elif xChar == "B" or xChar == "b":
+                    xCoord = 1
+                elif xChar == "C" or xChar == "c":
+                    xCoord = 2
+                elif xChar == "D" or xChar == "d":
+                    xCoord = 3
+                elif xChar == "E" or xChar == "e":
+                    xCoord = 4
+                elif xChar == "F" or xChar == "f":
+                    xCoord = 5
+                elif xChar == "G" or xChar == "g":
+                    xCoord = 6
+                elif xChar == "H" or xChar == "h":
+                    xCoord = 7
+                elif xChar == "I" or xChar == "i":
+                    xCoord = 8
+                elif xChar == "J" or xChar == "j":
+                    xCoord = 9
+                else:
+                    print("Invalid input. Try again")
+                    repeat = True
+            yCoord = int(input('Enter a row [1-10] to place ship: '))
+            yCoord = yCoord - 1
+            parts.append((xCoord, yCoord))
+            print()
+        parts = canonicalizeParts(parts)
+
+    #exceptions
     if not isAdjacent(parts):
             raise Exception()
     if player == 1:
@@ -115,6 +122,6 @@ def placeShip(player, shipCount):
         placeBody(parts,p1shipArr)
     else:
         #Player 2
-        if overlaps(parts, p1shipArr):
+        if overlaps(parts, p2shipArr):
             raise Exception()
         placeBody(parts, p2shipArr)
