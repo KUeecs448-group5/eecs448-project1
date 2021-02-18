9d2a9daf2ab4aaa4080f913ecfada7f03fd59e3d
w864s382 <w864s382@ku.edu>
2021-02-18 12:18:22 -0600

    Finished up user input

--- shipPlacement2.py
+++ shipPlacement2.py
@@ -26,15 +26,21 @@ p2shipArr = [
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
             ]
 
+#declairing the coordinate variables outside of a definition so they have a larger scope.
+xCoord = 0
+yCoord = 0
+face = 0
 
-#call to place all ships for one player
-def placeShip(player, shipCount):
-  #player 1 = 0, player 2 = 1
-  for i in range(1, shipCount+1): #i = size of ship curently placing
+#made userInput function, so it can easily be called again in the event of a misplaced ship
+def userInput(i):
     print('Placing Size ' + str(i) + ' Ship')
     orient = input('Horizontal or Vertical?(H/V): ')#code for verifing valid h or v for orientation
-    while not((orient == 'H') or (orient == 'V') or (orient == 'h') or (orient == 'vhh')):
+    while not((orient == 'H') or (orient == 'V') or (orient == 'h') or (orient == 'v')):
       orient = input('Invalid Character Please Enter "H" or "V": ')
+    if orient == "H" or orient == "h":#convert orient into a number, 0 is horizontal, 1 is vertical
+      face = 0
+    elif xChar == "V" or xChar == "v":
+      face = 1
     print("Please give the leftmost/topmost node of your ship")
     xChar = input('Which column [A-J]?: ') #code for x coordinate
     repeat = True
@@ -63,3 +69,21 @@ def placeShip(player, shipCount):
       else:
         xChar = input('Invalid input. Please enter a valid column [A-J]: ')
         repeat = True
+    yChar = input('Which Row [1-10]?: ') #user input for y
+    repeat = True
+    while repeat == True:
+      if not yChar.isnumeric(): #make sure it is a number
+        yChar = input('Invalid input. Please enter a valid row [1-10]: ')
+      elif not ((int(yChar) > 0) and (int(yChar) < 11)):#verification
+        yChar = input('Invalid input. Please enter a valid row [1-10]: ')
+      else:
+        repeat = False
+    yCoord = int(yChar) - 1 #change to 0-9
+
+
+#call to place all ships for one player
+def placeShip(player, shipCount):
+  #player 1 = 0, player 2 = 1
+  for i in range(1, shipCount+1): #i = size of ship curently placing
+    userInput(i)#fetch orientation and coordinates
+    print(face)
