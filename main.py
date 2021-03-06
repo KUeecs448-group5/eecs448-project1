29a9fff2a44063acace421487654901f0043ecbf
Aaron Harpt <a169h161@ku.edu>
2021-03-06 13:28:40 -0600

    Replace main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #Name: Caden Kroonenberg
 
-
-import Print, shotDetection, shipPlacement2, aiPlacement, easyShotDetection
+import Print, shotDetection, shipPlacement2
 from termcolor import colored
 
 def run(shipCount):
@@ -54,56 +53,6 @@ def run(shipCount):
                 player = 1
                 print(chr(27) + "[2J")
 
-def aiRun(shipCount):
-    """
-    implements every method of battleship game and checks for winner every turn
-    shipCount: number of ships(potentially of various sizes) that each player has.
-    Precondition: player variable initialized.
-    Postcondition: checks win condition every turn and declares winner when one player has no ships remaining.
-    """
-    endGame = False
-    player = 1
-
-    print()
-    print("Player 1:")
-    print()
-    shipPlacement2.placeShip(0, shipCount)
-    player = 2
-    input("Press Enter and then switch players to continue...")
-    print(chr(27) + "[2J")
-    print("Player 2:")
-    print()
-    aiPlacement.placeShip(shipCount)
-    player = 1
-    input("Press Enter and then switch players to continue...")
-    print(chr(27) + "[2J")
-
-    while not endGame:
-        if player == 1:
-            print("Player 1:")
-        elif player == 2:
-            print("Player 2:")
-        Print.printTopMap(player)
-        print()
-        Print.printBottomMap(player)
-        easyShotDetection.shot(player)
-
-        #check win condition and switch players if not
-        if player == 1:
-            if shotDetection.p1shotCount >= winCount:
-                endGame = True
-                print("\nPlayer 1 Wins!\n")
-            else:
-                player = 2
-                print(chr(27) + "[2J")
-        elif player == 2:
-            if shotDetection.p2shotCount == winCount:
-                endGame = True
-                print("\nPlayer 2 Wins!\n")
-            else:
-                player = 1
-                print(chr(27) + "[2J")
-
 print(chr(27) + "[2J")
 print(colored("BATTLESHIP", 'blue', 'on_grey'))
 print()
@@ -121,4 +70,4 @@ while repeat == True:
         for i in range(1,int(shipCount)+1):
           winCount = winCount + i
 
-aiRun(shipCount)
+run(shipCount)
