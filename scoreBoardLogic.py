d2971abdbf2e22aa6266265834aab9bc203936c4
Nick Velicer <njvelicer@ku.edu>
2021-03-13 11:54:36 -0600

    added print functionality, adjusted score representation

--- scoreBoardLogic.py
+++ scoreBoardLogic.py
@@ -4,12 +4,19 @@
 
 startingArray1 = [[]]
 startingArray2 = [[]]
-
+initialShipNumber1 = 0
+initialShipNumber2 = 0
 
 #fills a local variable with the initial placements for the players/ai
 def initializeScore(arr1, arr2):
+    global startingArray1
+    global startingArray2
+    global initialShipNumber1
+    global initialShipNumber2
     startingArray1 = arr1
     startingArray2 = arr2
+    initialShipNumber1 = spotsRemaining(startingArray1)
+    initialShipNumber2 = spotsRemaining(startingArray2)
 
 
 #takes in an initialized 10x10 2d array
@@ -24,9 +31,24 @@ def spotsRemaining(array):
 
 #takes in updated player/ai arrays, returns [score1, score2]
 def returnScores(current1, current2):
-    shotDiff1 = spotsRemaining(current1)
-    shotDiff2 = spotsRemaining(current2)
+    shotDiff1 = initialShipNumber1 - spotsRemaining(current1)
+    shotDiff2 = initialShipNumber2 - spotsRemaining(current2)
     return [shotDiff1, shotDiff2]
+
+
+#prints a command line representation of each player's score based on their current board states
+def printScoreboard(array1, array2, aiCheck):
+    print('')
+    print('        Scores:       ')
+    print('----------------------')
+    if aiCheck:
+        print('Player: ', returnScores(array1, array2)[0], "/", initialShipNumber1)
+        print('AI: ', returnScores(array1, array2)[1], "/", initialShipNumber2)
+    else:
+        print('Player 1: ', returnScores(array1, array2)[0], "/", initialShipNumber1)
+        print('Player 2: ', returnScores(array1, array2)[1], "/", initialShipNumber2)
+    print('----------------------')
+    print('')
 
 
 
